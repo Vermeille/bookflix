2929ba7e4084db2b191b952d32744a6f37cb0c7a
Guillaume "Vermeille" Sanchez <guillaume.v.sanchez@gmail.com>
2026-08-28 00:26:53 +0200

    FUCK YOU GOOGLE FUCK YOU FUCK YOU

--- bookflix/book_utils.py
+++ bookflix/book_utils.py
@@ -1,4 +1,9 @@
-from isbnlib import canonical, meta, cover
+import os
+import time
+from isbnlib import canonical, meta, cover, config
+from isbnlib.dev import ISBNLibHTTPError
+
+config.add_apikey("goob", os.environ["GOOGLE_BOOKS_API_KEY"])
 
 
 def canonical_isbn(isbn):
@@ -13,4 +18,18 @@ def get_book_info_by_isbn(isbn):
     Fetches book information from an online source using the ISBN number.
     """
     isbn = canonical(isbn)
-    return {**meta(isbn), **cover(isbn)}
+    m = {}
+    for _ in range(3):
+        try:
+            m = meta(isbn)
+            break
+        except ISBNLibHTTPError:
+            time.sleep(1)
+    c = {}
+    for _ in range(3):
+        try:
+            c = cover(isbn)
+            break
+        except ISBNLibHTTPError:
+            time.sleep(1)
+    return {**m, **c}
